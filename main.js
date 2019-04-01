fb94159b6e22c338e6aeea17de12ecc48111aed5
silashuereca <silasgranthuereca@gmail.com>
2019-04-01 14:55:32 -0500

    Added in comment for JS file code

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+
+//Slide in full screen navigation menu
 function openNav() {
   document.getElementById('myNav').style.width = "100%";
 }
-
 function closeNav() {
   document.getElementById('myNav').style.width = "0%";
 }
