8b348f0fc561898259d0332bb8914b02939cd34b
silashuereca <silasgranthuereca@gmail.com>
2019-05-09 00:56:23 -0500

    Added Jquery for first auto slideshow

--- main.js
+++ main.js
@@ -1,84 +1,70 @@
-//Selecting the closebtn for the overlay menu
-let navObj = {
-  overlayNavContainer: document.getElementById('myNav'),
-  menuBars: document.getElementById('icon')
-}
+// //Selecting the closebtn for the overlay menu
+// let navObj = {
+//   overlayNavContainer: document.getElementById('myNav'),
+//   menuBars: document.getElementById('icon')
+// }
 
 
-function openNav() {
-  navObj.overlayNavContainer.style.width = '100%';
-  navObj.menuBars.style.display = 'none';
-}
+// function openNav() {
+//   navObj.overlayNavContainer.style.width = '100%';
+//   navObj.menuBars.style.display = 'none';
+// }
 
-function closeNav() {
-  navObj.overlayNavContainer.style.width = '0';
-  navObj.menuBars.style.display = 'block';
-}
-
-
+// function closeNav() {
+//   navObj.overlayNavContainer.style.width = '0';
+//   navObj.menuBars.style.display = 'block';
+// }
 
 
-//Main welcome page automated slider 
-let index = 0;
-let images = [];
-let time = 4000;
 
 
-// Image List 
-images[0] = '../PhotosPhotos/1833 W. Park Place Photos/03_1833_W_Park_Place_003_mls.jpg';
-images[1] = '../Photos/1833 W. Park Place Photos/04_1833_W_Park_Place_004_mls.jpg';
-images[2] = '../Photos/1833 W. Park Place Photos/05_1833_W_Park_Place_005_mls.jpg';
+//Main welcome page automated slider with Jquery
+$(document).ready(function(){
 
-// Change Image
-function changeImg() {
-  document.slide.className += 'fadeOut';
-  document.slide.src = images[index]
-  setTimeout(function(){
-    document.slide.src = images[index];
-    document.slide.className = "";
-  }, 1000)
-  index++;
-  if (index == images.length){index = 0;} 
+  $('#autoSlideShow > div:gt(0)').hide();
 
-  setTimeout(changeImg, time)
-  
-}
-changeImg();
+  setInterval(function(){
+    $('#autoSlideShow > div:first')
+    .fadeOut(300)
+    .next()
+    .fadeIn(300)
+    .end()
+    .appendTo('#autoSlideShow');
+  }, 3000);
+
+});
 
 
 
 
 
+// //Selecting second slideshow images tag 
+// const propTwoImages = document.getElementById('propTwoImages');
 
+// //Array of second slideshow of images
+// let propTwoImagesArray = 
+// ['../Home Page/3211 N McKinley Photos/01_3211_n_mckinley_ave_001_mls.jpg', 
+// '../Home Page/3211 N McKinley Photos/02_3211_n_mckinley_ave_002_mls.jpg', 
+// '../Home Page/3211 N McKinley Photos/03_3211_n_mckinley_ave_003_mls.jpg', 
+// '../Home Page/3211 N McKinley Photos/04_3211_n_mckinley_ave_004_mls.jpg'];
 
+// // Second property index number 
+// let propTwoIndex = 0;
 
-//Selecting second slideshow images tag 
-const propTwoImages = document.getElementById('propTwoImages');
+// // Loads second property image 
+// function loadSecondPropImg (img) {
+//   propTwoImages.src = img;
+// }
 
-//Array of second slideshow of images
-let propTwoImagesArray = 
-['../Home Page/3211 N McKinley Photos/01_3211_n_mckinley_ave_001_mls.jpg', 
-'../Home Page/3211 N McKinley Photos/02_3211_n_mckinley_ave_002_mls.jpg', 
-'../Home Page/3211 N McKinley Photos/03_3211_n_mckinley_ave_003_mls.jpg', 
-'../Home Page/3211 N McKinley Photos/04_3211_n_mckinley_ave_004_mls.jpg'];
+// // Changes index number when clicked
+// function propTwoChangeImg(direction){
+//   propTwoIndex += (direction === 'next' ? 1 : -1);
 
-// Second property index number 
-let propTwoIndex = 0;
+//   if (propTwoIndex === propTwoImagesArray.length) {propTwoIndex = 0}
+//   else if (propTwoIndex < 0) {propTwoIndex = propTwoImagesArray.length - 1}
 
-// Loads second property image 
-function loadSecondPropImg (img) {
-  propTwoImages.src = img;
-}
+//   loadSecondPropImg(propTwoImagesArray[propTwoIndex])
+// }
 
-// Changes index number when clicked
-function propTwoChangeImg(direction){
-  propTwoIndex += (direction === 'next' ? 1 : -1);
-
-  if (propTwoIndex === propTwoImagesArray.length) {propTwoIndex = 0}
-  else if (propTwoIndex < 0) {propTwoIndex = propTwoImagesArray.length - 1}
-
-  loadSecondPropImg(propTwoImagesArray[propTwoIndex])
-}
-
-//this calls to load the first image in second slide show
-loadSecondPropImg(propTwoImagesArray[propTwoIndex]);
+// //this calls to load the first image in second slide show
+// loadSecondPropImg(propTwoImagesArray[propTwoIndex]);
